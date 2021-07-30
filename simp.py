a289553e8295296b70f86db9d927e5b5124d389d
will <jaskamiin@gmail.com>
2021-07-30 11:43:31 -0400

    boilerplate simp and trivia classes

---
+++ simp.py
@@ -0,0 +1,10 @@
+# create instances of Simptitude
+
+class Simp():
+    def __init__(self, goddess=None):
+        self.goddess = goddess
+
+    # [testing only] put her on a pedestal
+    def simpery(self):
+        pass
+        
