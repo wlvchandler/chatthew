c11813c19502550e67e3ef756f197bec77e53c26
hypehypehypehypehypehype <jrockerondrums@gmail.com>
2021-08-06 16:33:58 -0500

    Improving Trivia a bit

--- trivia.py
+++ trivia.py
@@ -1,4 +1,5 @@
 import random
+import string
 
 from questions import questions
 
@@ -7,9 +8,22 @@ trivia_map = {
 }
 
 
+def get_question(question_dict):
+    return question_dict['Q']
+
+
+def get_options(question_dict):
+    return ''.join(['{}. {}\n'.format(option, answer)
+                    for (option, answer) in zip(string.ascii_lowercase, question_dict['O'])])
+
+
+def get_answer(question_dict):
+    return '||{}||'.format(question_dict['A'])
+
+
 def generate_question(subcommand):
     question = random.choice(trivia_map[subcommand])
-    return random.choice(trivia_map[subcommand])
+    return get_question(question) + '\n' + str(get_options(question)) + '\n' + get_answer(question) + '\n'
 
 
 class Trivia():
